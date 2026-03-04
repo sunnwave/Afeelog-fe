394c7c2ce985902413be84fa5b08de32159d8ea2
sunnwave <ssun20886@gmail.com>
2026-03-04 15:04:07 +0900

    ✨[Feat] 회원가입 기능 구현

--- src/components/features/auth/sign-up/SignupPage.tsx
+++ src/components/features/auth/sign-up/SignupPage.tsx
@@ -3,9 +3,13 @@ import { useNavigation } from "@/shared/hooks/ui/useNavigation";
 import SignupForm from "./signupForm/SignupForm";
 import SignupTop from "./SignupTop";
 import SignupBottom from "./SignupBottom";
+import { useCreateUser } from "./hooks/useCreateUser";
 
 export default function SignupPage() {
   const { onClickNavigation } = useNavigation();
+
+  const { onCreateUser } = useCreateUser();
+
   return (
     <div className="min-h-screen bg-background overflow-x-hidden px-2 py-4 lg:px-6 lg:pb-8">
       {/* TODO: 뒤로가기 경로 설정 */}
@@ -13,7 +17,7 @@ export default function SignupPage() {
 
       <main className="flex-1 flex flex-col items-center px-4 py-8 pb-20 gap-10">
         <SignupTop />
-        <SignupForm />
+        <SignupForm onSubmit={onCreateUser} />
         <SignupBottom onClickNavigation={() => onClickNavigation("/login")()} />
       </main>
     </div>
---
+++ src/components/features/auth/sign-up/hooks/useCreateUser.ts
@@ -0,0 +1,53 @@
+import { useToast } from "@/components/commons/toast/ToastProvider";
+import {
+  IMutation,
+  IMutationCreateUserArgs,
+} from "@/shared/graphql/generated/types";
+import { gql, useMutation } from "@apollo/client";
+
+const CREATE_USER = gql`
+  mutation createUser($createUserInput: CreateUserInput!) {
+    createUser(createUserInput: $createUserInput) {
+      _id
+      email
+      name
+    }
+  }
+`;
+
+type CreateUserForm = {
+  email: string;
+  name: string;
+  password: string;
+};
+
+export const useCreateUser = () => {
+  const [createUser, { loading }] = useMutation<
+    Pick<IMutation, "createUser">,
+    IMutationCreateUserArgs
+  >(CREATE_USER);
+
+  const { error, success } = useToast();
+
+  const onCreateUser = async (form: CreateUserForm) => {
+    try {
+      const res = await createUser({
+        variables: {
+          createUserInput: {
+            email: form.email,
+            name: form.name,
+            password: form.password,
+          },
+        },
+      });
+      success("회원가입이 완료되었습니다👐🏻✨");
+      console.log(res.data?.createUser);
+    } catch (err) {
+      if (err instanceof Error) {
+        error(err.message || "회원가입에 실패했습니다😢");
+        console.error(err);
+      }
+    }
+  };
+  return { onCreateUser, loading };
+};
