656b9d9e00f6f9f29450b394138b800f579dc509
sunnwave <ssun20886@gmail.com>
2026-03-06 22:59:07 +0900

    ✨[Feat] 로그인 기능 구현

--- src/components/features/auth/login/LoginScreen.tsx
+++ src/components/features/auth/login/LoginScreen.tsx
@@ -2,11 +2,12 @@ import BackButton from "@/components/commons/backButton/BackButton";
 import LoginForm from "./loginForm/LoginForm";
 import LoginBottom from "./LoginBottom";
 import { useNavigation } from "@/shared/hooks/ui/useNavigation";
+import useLoginUser from "@/shared/hooks/auth/useLoginUser";
 
 export default function LoginScreen() {
   const { onClickNavigation } = useNavigation();
 
-  const onCreateUser = () => {};
+  const { onLoginUser } = useLoginUser();
 
   return (
     <div className="min-h-screen bg-background overflow-x-hidden px-2 py-4 lg:px-6 lg:pb-8">
@@ -22,7 +23,7 @@ export default function LoginScreen() {
             공연의 감동을 기록하고 공유해보세요
           </p>
         </div>
-        <LoginForm onSubmit={onCreateUser} />
+        <LoginForm onSubmit={onLoginUser} />
         <LoginBottom
           onClickNavigation={() => onClickNavigation("/login/signup")()}
         />
---
+++ src/shared/hooks/auth/useFetchUserLoggedInLazy.ts
@@ -0,0 +1,29 @@
+import { gql, useLazyQuery } from "@apollo/client";
+import { IQuery } from "@/shared/graphql/generated/types";
+
+export const FETCH_USER_LOGGED_IN = gql`
+  query fetchUserLoggedIn {
+    fetchUserLoggedIn {
+      _id
+      email
+      name
+      picture
+      createdAt
+    }
+  }
+`;
+
+export function useFetchUserLoggedInLazy() {
+  const [fetchUserLoggedIn, { data, loading, error }] = useLazyQuery<
+    Pick<IQuery, "fetchUserLoggedIn">
+  >(FETCH_USER_LOGGED_IN, {
+    fetchPolicy: "network-only",
+  });
+
+  const run = async () => {
+    const res = await fetchUserLoggedIn();
+    return res.data?.fetchUserLoggedIn ?? null;
+  };
+
+  return { run, data, loading, error };
+}
